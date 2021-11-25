4c8546d107da34b753da21ad7fe4018b83360378
eristoddle <stephanmil@gmail.com>
2021-11-24 20:43:08 -0600

    Add Dropdown

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Dropdown from './src/components/Dropdown';
 
 export default function App() {
+  const [selected, setSelected] = useState(undefined);
+  const data = [
+    { label: 'One', value: '1' },
+    { label: 'Two', value: '2' },
+    { label: 'Three', value: '3' },
+    { label: 'Four', value: '4' },
+    { label: 'Five', value: '5' },
+  ];
+
   return (
     <View style={styles.container}>
-      <Dropdown label={'Select Item'} />
+      {!!selected && (
+        <Text>
+          Selected: label = {selected.label} and value = {selected.value}
+        </Text>
+      )}
+      <Dropdown label={'Select Item'} data={data} onSelect={setSelected} />
       <Text>This is the rest of the form.</Text>
       <StatusBar style="auto" />
     </View>
@@ -19,5 +33,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
   },
 });
--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,58 +1,115 @@
-import React, { FC, useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React, { FC, ReactElement, useRef, useState } from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Modal,
+  View,
+} from 'react-native';
 import { Icon } from 'react-native-elements';
 
 interface Props {
   label: string;
+  data: Array<{ label: string; value: string }>;
+  onSelect: (item: { label: string; value: string }) => void;
 }
 
-const Dropdown: FC<Props> = ({ label }) => {
+const Dropdown: FC<Props> = function ({ label, data, onSelect }) {
+  const DropdownButton = useRef();
   const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(undefined);
+  const [dropdownTop, setDropdownTop] = useState(0);
 
-  const toggleDropdown = () => {
-    setVisible(!visible);
+  const toggleDropdown = (): void => {
+    visible ? setVisible(false) : openDropdown();
   };
 
-  const renderDropdown = () => {
-    if (visible) {
-      return (
-          <Text style={styles.dropdown}>
-            This is where the dropdown will be rendered.
-          </Text>
-      );
-    }
+  const openDropdown = (): void => {
+    DropdownButton.current.measure((_fx, _fy, _w, h, _px, py) => {
+      setDropdownTop(py + h);
+    });
+    setVisible(true);
+  };
+
+  const onItemPress = (item): void => {
+    setSelected(item);
+    onSelect(item);
+    setVisible(false);
+  };
+
+  const renderItem = ({ item }): ReactElement<any, any> => (
+    <TouchableOpacity style={styles.item} onPress={() => onItemPress(item)}>
+      <Text>{item.label}</Text>
+    </TouchableOpacity>
+  );
+
+  const renderDropdown = (): ReactElement<any, any> => {
+    return (
+      <Modal visible={visible} transparent animationType="none">
+        <TouchableOpacity
+          style={styles.overlay}
+          onPress={() => setVisible(false)}
+        >
+          <View style={[styles.dropdown, { top: dropdownTop }]}>
+            <FlatList
+              data={data}
+              renderItem={renderItem}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </View>
+        </TouchableOpacity>
+      </Modal>
+    );
   };
 
   return (
     <TouchableOpacity
+     ref={DropdownButton}
       style={styles.button}
       onPress={toggleDropdown}
     >
       {renderDropdown()}
-      <Text style={styles.buttonText}>{label}</Text>
-      <Icon type='font-awesome' name="chevron-down"/>
+      <Text style={styles.buttonText}>
+        {(selected && selected.label) || label}
+      </Text>
+      <Icon style={styles.icon} type="font-awesome" name="chevron-down" />
     </TouchableOpacity>
   );
-}
+};
 
 const styles = StyleSheet.create({
   button: {
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#efefef",
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#efefef',
     height: 50,
-    width: "90%",
-    paddingHorizontal: 10,
     zIndex: 1,
   },
   buttonText: {
     flex: 1,
-    textAlign: "center",
+    textAlign: 'center',
+  },
+  icon: {
+    marginRight: 10,
   },
   dropdown: {
-    position: "absolute",
+    position: 'absolute',
     backgroundColor: '#fff',
-    top: 50,
+    width: '100%',
+    shadowColor: '#000000',
+    shadowRadius: 4,
+    shadowOffset: { height: 4, width: 0 },
+    shadowOpacity: 0.5,
+  },
+  overlay: {
+    width: '100%',
+    height: '100%',
+  },
+  item: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
   },
 });
 
